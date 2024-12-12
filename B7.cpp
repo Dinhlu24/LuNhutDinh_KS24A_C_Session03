c7c915ad34ac7772c98e99bd5c2fa11845082cdc
Dinhlu24 <dinhlunhut@gmail.com>
2024-12-12 09:41:51 +0700

    B7.cpp (Fixed)

--- B7.cpp
+++ B7.cpp
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main(){
-	printf("Moi nhap vao mot so nguyen co 4 chu so");
-	int n,sum=0;	scanf("%d", &n);
-	while(n){
-		int x = n%10;
-		sum += x;
-		n/=10;
-	}
-	printf("Tong cac chu so do la %d", sum);
+	printf("Moi nhap vao mot so nguyen co 4 chu so ");
+	int n,sum=0,num1,num2,num3;	scanf("%d", &n);
+	num1 = n%10;
+	n/=10;
+	num2=n%10;
+	n/=10;
+	num3 = n%10;
+	n/=10;
+	sum += num1 + num2 + num3 + n;
+	printf("Tong cac chu so do la %d ", sum);
 }
